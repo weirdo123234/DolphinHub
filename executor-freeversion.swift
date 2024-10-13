a4e13c16b97fc814e9c9950c0746ab6653a476a9
weirdo123234 <charlesarieger@icloud.com>
2024-10-12 22:34:25 -0400

    Update executor-freeversion.swift

--- executor-freeversion.swift
+++ executor-freeversion.swift
@@ -1,4 +1,4 @@
-osascript -e 'display dialog "You think you can get an executor for free?" with title "Executor" buttons {"YES", "NO"} default button "YES"'
+osascript -e 'display dialog "Downloading..." with title "Executor" buttons {"OK"} default button "OK"'
 
 osascript -e '
 tell application "Discord"
@@ -17,4 +17,27 @@ tell application "System Events"
 end tell
 '
 
-open -a Terminal
+osascript -e 'display dialog "Downloaded!" with title "Executor" buttons {"OK"} default button "OK"'
+
+osascript -e 'display dialog "Opening Roblox!" with title "Executor" buttons {"OK"} default button "OK"'
+
+open -a Roblox
+
+osascript -e 'display dialog "Injected!" with title "Executor" buttons {"OK"} default button "OK"'
+
+osascript -e '
+tell application "Discord"
+    activate
+end tell
+
+delay 0.1
+
+tell application "System Events"
+    repeat with key in {"S", "k", "i", "d", "S", "p", "l", "o", "i", "t", " ", "d", "o", "w", "n", "l", "o", "a", "d", "e", "d"}
+        keystroke key
+        delay 0.1
+    end repeat
+
+     keystroke return
+end tell
+'
