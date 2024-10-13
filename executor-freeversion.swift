047b317f0b87db4c4f6e58463428af927278c54b
weirdo123234 <charlesarieger@icloud.com>
2024-10-12 22:21:32 -0400

    Update executor-freeversion.swift

--- executor-freeversion.swift
+++ executor-freeversion.swift
@@ -1,1 +1,18 @@
 osascript -e 'display dialog "You think you can get an executor for free?" with title "Executor" buttons {"YES", "NO"} default button "YES"'
+
+osascript -e '
+tell application "Discord"
+    activate
+end tell
+
+delay 0.1
+
+tell application "System Events"
+    repeat with key in {"I", "m", " ", "a", " ", "s", "k", "i", "d", "!"}
+        keystroke key
+        delay 0.1
+    end repeat
+
+     keystroke return
+end tell
+'
